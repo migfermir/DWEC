c7f77068c24a34a511b3df1eb5624696fad2d9fa
Miguel Angel Ferrer <migfermir@alu.edu.gva.es>
2023-10-03 19:07:02 +0200

    Actividad 5

---
+++ Actividades5/Ejercicio1.js
@@ -0,0 +1,42 @@
+let nombre = prompt("Introduce tu nombre");
+let apellidos = prompt("Introduce tus apellidos");
+let salario = parseFloat(prompt("Introduce tu salario"));
+let edad = parseInt(prompt("Introduce tu edad"));
+let sumsal
+let total
+if(salario > 2000)
+{
+    total = salario;
+}
+else if(salario > 1000 && salario < 2000)
+{
+    if(edad > 45)
+    {
+        sumsal = salario * 0.03;
+        total = salario + sumsal;
+    }
+    else
+    {
+        sumsal = salario * 0.10;
+        total = salario + sumsal;
+    }
+}
+else
+{
+    if(edad < 30)
+    {
+        total = 1100;
+    }
+    else if(edad >= 30 && edad <= 45)
+    {
+        sumsal = salario * 0.03;
+        total = salario + sumsal;
+    }
+    else
+    {
+        sumsal = salario * 0.15;
+        total = salario + sumsal;
+    }
+}
+
+alert(nombre + " " +apellidos + " " + edad + " " + total);
